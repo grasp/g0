418442c0aaa1d638c45ccf3263865d22beb3234d
hunter <hunter@HD64.(none)>
2011-06-05 16:51:50 +0800

    change for development

--- lib/tasks/mongo.rb
+++ lib/tasks/mongo.rb
@@ -1,7 +1,13 @@
 # To change this template, choose Tools | Templates
 # and open the template in the editor.
 
+if ARGF.argv[0]=="development"
 $debug=true
+else
+  $debug=false
+end
+#puts "$debug=#{$debug}"
+
 $mongo=Mongo::Connection.new('localhost', 27017)
 $debug ? $db = $mongo.db('g0_development') : $db = $mongo.db('g0_production')
 Mongoid.database = $db
