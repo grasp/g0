36fac14d78bc8e10a70855bcdc5d5237cf286497
unknown <w22812@.ds.mot.com>
2011-06-20 18:32:05 +0800

    add public cargo case

--- test/watir/users/user_page_test.rb
+++ test/watir/users/user_page_test.rb
@@ -10,24 +10,12 @@ if false
   require "test/unit"
   puts "load test env cost #{Time.now-start_time} sec"
 end
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
-  # fixtures :all
-  # Add more helper methods to be used by all tests here...
-  $browser = Watir::Browser.new()
-  $browser.minimize
-  $site_root= "http://localhost:5678"
-  $user_name=Time.now.to_s.gsub(/\s|:|\+/,"").to_s
-  $user_email="#{$user_name}@gmail.com"
-  $mobile_phone=$user_name.slice(9,18)
-end
+
 
 class W090UserPageTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "0-any_user_should_see_first_page" do
+  
+test "user_register_login_logout" do 
+  #"0-any_user_should_see_first_page"
     puts "0any user should see first page"
     #open index
     b=$browser.goto "#{$site_root}"
@@ -38,9 +26,9 @@ class W090UserPageTest < ActiveSupport::TestCase
     ma.each do |text|
       assert result.include?(text),"#{text} 不存在 !!"
     end
-  end
 
-  test "1-user open register form" do
+
+  #"1-user open register form" 
     puts "1-user open register form"
     b=$browser.link(:href, "#{$site_root}/users/new").click
     assert b
@@ -49,9 +37,8 @@ class W090UserPageTest < ActiveSupport::TestCase
     ma.each do |text|
       assert result.include?(text),"#{text} 不存在 !!"
     end
-  end
 
-  test "2-user click button without any fill" do
+  #"2-user click button without any fill"
     puts "2user click button without any fill"
     $browser.link(:href, "#{$site_root}/users/new").click
     b=$browser.button(:id, "user_submit").click
@@ -61,9 +48,9 @@ class W090UserPageTest < ActiveSupport::TestCase
     ma.each do |text|
       assert result.include?(text),"#{text} 不存在 !!"
     end
-  end
 
-  test "3-user name should unique when create" do
+
+#"3-user name should unique when create" 
     puts "3user name should unique when create"
     #admin create should fail
     sleep 1
@@ -80,10 +67,11 @@ class W090UserPageTest < ActiveSupport::TestCase
     ma.each do |text|
       assert result.include?(text),"#{text} 不存在 !!"
     end
-  end
+
+#user register and login" 
+         puts "00000-user register and login"
+     $browser.goto("#{$site_root}")
  
-  test "4user register and login" do
-    puts "4user register and login"
     $browser.link(:href, "#{$site_root}/users/new").click
     $browser.text_field(:id, "user_name").set($user_name)
     $browser.text_field(:id, "user_email").set($user_email)
@@ -108,16 +96,23 @@ class W090UserPageTest < ActiveSupport::TestCase
     $browser.text_field(:id, "email").set($user_name)
     $browser.text_field(:id, "password").set("1234567")
     $browser.button(:value, "登录").click
+    sleep 1
     result=$browser.text.force_encoding('utf-8')
     ["登出","我的货源","我的车源","关于物流零距离"].each { |text| assert result.include?(text),"#{text} 不存在 !!"}
     sleep(2) #need wait enough time here
     $browser.link(:href, "#{$site_root}/users/logout").click
     sleep(1)
-  end
+ 
 
-  test "5repeated username register" do
+ #"5repeated username register" 
     puts " 5repeated username register"
-    sleep 1
+    
+   result=$browser.text.force_encoding('utf-8')
+    if result.include?("登出")
+      $browser.link(:href, "#{$site_root}/users/logout").click
+    end
+    
+    sleep 2
     $browser.link(:href, "#{$site_root}/users/new").click
     $browser.text_field(:id, "user_name").set($user_name)
     $browser.text_field(:id, "user_email").set("#{$user_name+"1"}@gmail.com")
@@ -127,9 +122,9 @@ class W090UserPageTest < ActiveSupport::TestCase
     $browser.button(:id, "user_submit").click
     result=$browser.text.force_encoding('utf-8')
     ["该用户名已经存在"].each { |text| assert result.include?(text),"#{text} 不存在 !!"}
-  end
+
   
-  test "6repeated email regester" do
+ # "6repeated email regester" 
     puts "6 repeated email regester"
     $browser.link(:href, "#{$site_root}/users/new").click
     $browser.text_field(:id, "user_name").set($user_name.chop+"1")
@@ -140,9 +135,9 @@ class W090UserPageTest < ActiveSupport::TestCase
     $browser.button(:id, "user_submit").click
     result=$browser.text.force_encoding('utf-8')
     ["该email已经存在"].each { |text| assert result.include?(text),"#{text} 不存在 !!"}
-  end
 
-  test "7repeated mobile phone register" do
+
+ # "7repeated mobile phone register" 
     puts "7 repeated mobile phone register"
     $browser.link(:href,"#{$site_root}/users/new").click
     $browser.text_field(:id, "user_name").set($user_name.chop+"2")
@@ -150,19 +145,18 @@ class W090UserPageTest < ActiveSupport::TestCase
     $browser.text_field(:id, "user_password").set("1234567")
     $browser.text_field(:id, "user_password_confirmation").set("1234567")
     $browser.text_field(:id, "user_mobilephone").set($mobile_phone)
-      $browser.button(:id, "user_submit").click
-      result=$browser.text.force_encoding('utf-8')
-        ["该手机已经存在"].each { |text| assert result.include?(text),"#{text} 不存在 !!"}
-end
+    $browser.button(:id, "user_submit").click
+    result=$browser.text.force_encoding('utf-8')
+    ["该手机已经存在"].each { |text| assert result.include?(text),"#{text} 不存在 !!"}
 
-#login as email account
-test "8-login as email account" do
-$browser.link(:href, "#{$site_root}/users/login").click
-$browser.text_field(:id, "email").set($user_email)
-$browser.text_field(:id, "password").set("1234567")
-$browser.button(:value, "登录").click
-result=$browser.text.force_encoding('utf-8')
-["登出","我的货源","我的车源","关于物流零距离"].each { |text| assert result.include?(text),"#{text} 不存在 !!"}
-end
 
+ #"8-login as email account" 
+    $browser.link(:href, "#{$site_root}/users/login").click
+    $browser.text_field(:id, "email").set($user_email)
+    $browser.text_field(:id, "password").set("1234567")
+    $browser.button(:value, "登录").click
+    result=$browser.text.force_encoding('utf-8')
+    ["登出","我的货源","我的车源","关于物流零距离"].each { |text| assert result.include?(text),"#{text} 不存在 !!"}
+
+end
 end
--- test/watir/w090_watir_test.rb
+++ test/watir/w090_watir_test.rb
@@ -13,5 +13,26 @@ require "test/unit"
 puts "load test env cost #{Time.now-start_time} sec"
 #end
 
+class ActiveSupport::TestCase
+  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
+  #
+  # Note: You'll currently still have to declare fixtures explicitly in integration tests
+  # -- they do not yet inherit this setting
+  # fixtures :all
+  # Add more helper methods to be used by all tests here...
+  $browser = Watir::Browser.new()
+  $browser.minimize
+  $site_root= "http://localhost:5678"
+  $user_name=Time.now.to_s.gsub(/\s|:|\+/,"").to_s
+  $user_email="#{$user_name}@gmail.com"
+  $mobile_phone=$user_name.slice(9,18)
+  $browser.goto("#{$site_root}")
+  #each time run test, new a user
+  
+  puts "we got a new user =#{$user_name} with email=#{$user_email} and mobilephone=#{$mobile_phone}"
+
+  
+end
+require  File.dirname(__FILE__) + "/cargos/cargo_page_test"
 require  File.dirname(__FILE__) + "/users/user_page_test"
 
